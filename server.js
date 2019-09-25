9752fce8e696d1b04e390e546af081f3469b8bd9
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-24 20:08:50 -0700

    completed run-model and server

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require('./lib/connect')();
+const express = require('express');
+const app = express();
+const Band = require('./lib/model/bands');
+
+app.use(express.json());
+
+app.get('./api/bands', (req, res, next) => {
+  Band.find()
+    .then(bands => {
+      res.json(bands);
+    })
+    .catch(next);
+});
+
+app.get('./api/animals/:id', (req, res, next) => {
+  Band.findById(req.params.id)
+    .then(band => {
+      res.json(band);
+    })
+    .catch(next);
+});
+
+app.post('./api/bands', (req, res, next) => {
+  Band.findByIdAndRemove(req.params.id)
+    .then(removed => {
+      res.json(removed);
+    })
+    .catch(next);
+});
+
+app.listen(3000, () => console.log('server running on 3000'));
